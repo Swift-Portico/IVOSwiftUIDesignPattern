a39d314a06a2f89f2dfde32c747d3772fb591864
pradeepiOS <pradeep.iostech@gmail.com>
2021-11-02 19:11:31 +0530

    Initial Commit

---
+++ IVOSwiftUIDesignPattern/IVOSwiftUIDesignPatternApp.swift
@@ -0,0 +1,17 @@
+//
+//  IVOSwiftUIDesignPatternApp.swift
+//  IVOSwiftUIDesignPattern
+//
+//  Created by Pradeep's Macbook on 02/11/21.
+//
+
+import SwiftUI
+
+@main
+struct IVOSwiftUIDesignPatternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
